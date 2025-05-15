d75b7d21d852e0235f97444ed966e6da79349d37
ishi720 <ishizuka720@gmail.com>
2025-05-15 21:11:54 +0900

    タイポの修正

--- api/fetch_directory_images.php
+++ api/fetch_directory_images.php
@@ -4,31 +4,30 @@ $dir = "../img/" ;
 // 画像のパスを入れる配列
 $img_array = array();
 if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
-	$path = "";
-	while( ($file = readdir($handle)) !== false ) {
+    $path = "";
+    while( ($file = readdir($handle)) !== false ) {
+        $path = "./img/" . $file;
+        $path_info = pathinfo( $path );
 
-		$path = "./img/" . $file;
-		$path_info = pathinfo( $path );
-
-		//画像ファイルかチェックする
-		if( img_check($path_info['extension']) ) {
- 			$img_array[] = $path;
-		}
-	}
+        //画像ファイルかチェックする
+        if (img_check($path_info['extension'])) {
+            $img_array[] = $path;
+        }
+    }
 }
 
 //json形式で出力
 header('Content-type: application/json');
 $response = array();
-$response['response_deta'] = $img_array;
+$response['response_data'] = $img_array;
 echo json_encode($response);
 
 //画像チェック
 function img_check($extension){
-	switch ($extension) {
-	    case "jpg": return true;
-	    case "png": return true;
-	    case "gif": return true;
-	    default: return false;
-	}
+    switch ($extension) {
+        case "jpg": return true;
+        case "png": return true;
+        case "gif": return true;
+        default: return false;
+    }
 }
