9341582748ec19e56c0afd23257e98a69207686b
ishi720 <ishizuka720@gmail.com>
2025-06-04 01:13:01 +0900

    リファクタリング

--- api/fetch_directory_images.php
+++ api/fetch_directory_images.php
@@ -3,27 +3,25 @@
 $dir = "../img/" ;
 // 画像のパスを入れる配列
 $img_array = array();
-if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
-    $path = "";
+if (is_dir($dir) && $handle = opendir($dir)) {
     while( ($file = readdir($handle)) !== false ) {
-        $path = "./img/" . $file;
-        $path_info = pathinfo( $path );
-
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
         //画像ファイルかチェックする
-        if (img_check($path_info['extension'])) {
-            $img_array[] = $path;
+        if (img_check($extension)) {
+            $img_array[] = "./img/" . $file;
         }
     }
+    closedir($handle);
 }
 
 //json形式で出力
 header('Content-type: application/json');
-$response = array();
-$response['response_data'] = $img_array;
-echo json_encode($response);
+echo json_encode([
+    'response_data' => $img_array
+]);
 
 //画像チェック
-function img_check($extension){
+function img_check($extension) {
     $validExtensions = [
         'jpg', 'jpeg', 'png', 'gif', 'webp',
         'bmp', 'tiff', 'tif', 'svg', 'ico', 'heic', 'avif'
