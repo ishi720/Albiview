18048cf9738bcbb2fc90af27f135be299f82e5f3
ishi720 <ishizuka720@gmail.com>
2025-06-06 21:18:32 +0900

    厳格モード設定、ドックコメント追加

--- api/fetch_directory_images.php
+++ api/fetch_directory_images.php
@@ -1,8 +1,11 @@
 <?php
+
+declare(strict_types=1);
+
 // ディレクトリのパスを指定する
 $dir = "../img/" ;
 // 画像のパスを入れる配列
-$img_array = array();
+$img_array = [];
 if (is_dir($dir) && $handle = opendir($dir)) {
     while( ($file = readdir($handle)) !== false ) {
         $extension = pathinfo($file, PATHINFO_EXTENSION);
@@ -20,8 +23,13 @@ echo json_encode([
     'response_data' => $img_array
 ]);
 
-//画像チェック
-function img_check($extension) {
+/**
+ * 指定された拡張子が画像ファイル形式かどうかを判定
+ *
+ * @param string $extension ファイルの拡張子（例: "jpg", "png"）
+ * @return bool 画像ファイル形式の場合 true、それ以外は false
+ */
+function img_check(string $extension): bool {
     $validExtensions = [
         'jpg', 'jpeg', 'png', 'gif', 'webp',
         'bmp', 'tiff', 'tif', 'svg', 'ico', 'heic', 'avif'
