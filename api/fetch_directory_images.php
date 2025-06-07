ebef6ae6b70cbee0dffb9bd6bb8f0d8c134dbec5
ishi720 <ishizuka720@gmail.com>
2025-06-07 11:58:01 +0900

    画像一覧作成処理を関数化

--- api/fetch_directory_images.php
+++ api/fetch_directory_images.php
@@ -4,27 +4,34 @@ declare(strict_types=1);
 
 // ディレクトリのパスを指定する
 $dir = "../img/" ;
-// 画像のパスを入れる配列
-$img_array = [];
 
-// 画像ファイルのリストを作成
-if (is_dir($dir)) {
-    $files = scandir($dir);
-    foreach ($files as $file) {
-        if (is_file($dir . $file)) {
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-            //拡張子のチェック
-            if (img_check($extension)) {
-                $img_array[] = "./img/" . $file;
-            }
-        }
-    }
-}
 //json形式で出力
 header('Content-type: application/json');
 echo json_encode([
-    'response_data' => $img_array
+    'response_data' => getImageList($dir)
 ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+
+/**
+ * 指定されたディレクトリから画像ファイル一覧を取得
+ *
+ * @param string $dir ディレクトリパス
+ * @return array 画像ファイルの相対パス配列
+ */
+function getImageList(string $dir): array {
+    if (is_dir($dir)) {
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (is_file($dir . $file)) {
+                $extension = pathinfo($file, PATHINFO_EXTENSION);
+                //拡張子のチェック
+                if (isImageExtension($extension)) {
+                    $img_array[] = "./img/" . $file;
+                }
+            }
+        }
+    }
+    return $img_array;
+}
 
 /**
  * 指定された拡張子が画像ファイル形式かどうかを判定
@@ -32,7 +39,7 @@ echo json_encode([
  * @param string $extension ファイルの拡張子（例: "jpg", "png"）
  * @return bool 画像ファイル形式の場合 true、それ以外は false
  */
-function img_check(string $extension): bool {
+function isImageExtension(string $extension): bool {
     $validExtensions = [
         'jpg', 'jpeg', 'png', 'gif', 'webp',
         'bmp', 'tiff', 'tif', 'svg', 'ico', 'heic', 'avif'
