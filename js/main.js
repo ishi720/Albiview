ad94ca729ddb0bb19901ca723b87377dd8c1b10c
ishi720 <ishizuka720@gmail.com>
2025-06-09 22:41:05 +0900

    Pinterestの画像を表示できるようにする

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 "use strict"
 
+let showMode = "directory";
+let pint_user = "";
+
 $(function() {
-    $.ajax({
-        url: './api/fetch_directory_images.php',
-        type: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-    }).done(function(response){
-        $("#main").html(
-            $("#main_tmpl").render(response)
-        );
-        setTimeout( function(){
-            $('#content').isotope();
-        }, 500);
-    }).fail(function(){
-        console.error('画像取得失敗');
-    });
+    if (showMode == "directory") {
+        $.ajax({
+            url: './api/fetch_directory_images.php',
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+        }).done(function(response){
+            $("#main").html(
+                $("#main_tmpl").render(response)
+            );
+            setTimeout( function(){
+                $('#content').isotope();
+            }, 500);
+        }).fail(function(){
+            console.error('画像取得失敗');
+        });
+    } else {
+        $.ajax({
+            url: './api/fetch_pinterest_images.php?pint_user='+ pint_user,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function(response){
+            $("#main").html(
+                $("#pint_tmpl").render(response)
+            );
+            setTimeout( function(){
+                $('#content').isotope();
+            }, 500);
+        }).fail(function(){
+            console.error('画像取得失敗');
+        });
+    }
 });
